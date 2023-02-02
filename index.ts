038adc90632d081eea888bd6bc06a1b128a655ee
Konrad Cencelewicz <konrad.cencelewicz@gmail.com>
2023-02-02 05:48:09 +0100

    Added error handling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError} from "./utils/errors";
 
 const app = express();
 
@@ -9,6 +10,13 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+//Routes
+app.get('/', (req, res)  => {
+    throw new Error('Dammmmmmmmmm');
+})
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on 3001 port');
