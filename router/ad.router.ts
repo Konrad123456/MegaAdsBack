c7a0587a6e5d6ad7bf065c805261ddb5a7c56855
Konrad Cencelewicz <konrad.cencelewicz@gmail.com>
2023-02-09 20:43:53 +0100

    Added get/post routes

--- router/ad.router.ts
+++ router/ad.router.ts
@@ -1,6 +1,23 @@
+import { AdRecord } from './../records/ad.record';
 import { Router } from "express";
 
 export const adRouter = Router()
+    .get('/search/:name?', async (req, res) => {
+        const ads = await AdRecord.findAll(req.params.name ?? '');
+        return res.json(ads);
+    })    
+    
+    .get('/:id', async (req, res) => {
+        const ads = await AdRecord.getOne(req.params.id);
+        return res.json(ads);
+    })     
+    
+    .post('/', async (req, res) => {
+        const ads = new AdRecord(req.body);
+        await ads.insert();
+        return res.json(ads);
+    })    
+    
     .get('/', (req, res) => {
         console.log('Work');
     })
